0282c0bfa1671afaa558e22fe2d7ac73b7054339
NderKev <nderebakelvin@gmail.com>
2024-06-09 15:02:19 +0300

    decplmax-v1-bd-template

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+const configureRoutes = (app) => {
+  app.use('/api/auth', require('./api/auth'));
+  app.use('/api/users', require('./api/users'));
+  app.use('/api/mails', require('./api/mails'));
+  app.use('/', (req, res) => {
+    res.status(200).send('Welcome to Decplmax!');
+  });
+};
+
+module.exports = configureRoutes;
