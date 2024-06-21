615ab8f3925fa7ed79a49ff0120c3e4dc18d7df5
NderKev <nderebakelvin@gmail.com>
2024-06-21 14:20:51 +0300

    add-nft-smart-contracts

---
+++ contracts/NftFactory.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "./NftContarct.sol";
+
+contract NftFactory {
+    mapping(address => address) public usernameToNftCollectionAddress;
+
+    error CollectionExist();
+
+    event CollectionCreated(
+        address indexed collectionAddress,
+        address indexed collectionOwner
+    );
+
+    function createCollection(
+        string memory name_,
+        string memory symbol_
+    ) public {
+        if (usernameToNftCollectionAddress[msg.sender] != address(0)) {
+            revert CollectionExist();
+        }
+        NftContarct nftContarct = new NftContarct(name_, symbol_, msg.sender);
+        usernameToNftCollectionAddress[msg.sender] = address(nftContarct);
+        emit CollectionCreated(address(nftContarct), msg.sender);
+    }
+}
